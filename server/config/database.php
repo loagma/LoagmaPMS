48b6ffaf321cfe5170f8db481d1ed9f007bcc1ac
sparsh-15 <sparshsahu8435@gmail.com>
2026-02-27 15:55:31 +0530

    fix(database): Update database configuration and add SSL certificate
    - Replace SSL verification setting with SSL CA configuration in database settings
    - Adjust formatting in Redis prefix configuration for consistency
    - Add ISRG Root X1 SSL certificate for secure database connections

--- server/config/database.php
+++ server/config/database.php
@@ -59,8 +59,8 @@ return [
             'strict' => true,
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
-    PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
-]) : [],
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA', null),
+            ]) : [],
         ],
 
         'mariadb' => [
@@ -148,7 +148,7 @@ return [
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix' => env('REDIS_PREFIX', Str::slug((string) env('APP_NAME', 'laravel')).'-database-'),
+            'prefix' => env('REDIS_PREFIX', Str::slug((string) env('APP_NAME', 'laravel')) . '-database-'),
             'persistent' => env('REDIS_PERSISTENT', false),
         ],
 
